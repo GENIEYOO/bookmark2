08c05e303957e3b4979cc49facdb65609831e5ad
GENIEYOO <forstudym.m@gmail.com>
2018-03-14 21:16:47 +0900

    second commit

--- bookmark/views.py
+++ bookmark/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 #실제로 어떤 기능을 만들어 내는 부분
 #글을 쓸 때, 수정할 때, 지울 때, 볼 때 어떻게 해당기능이 동작할 것인지
-
+#crud django
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
